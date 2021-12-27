a371327fd6032da670b0b51b28a992a865074463
Japie Knouwds <japie.knouwds@nintex.com>
2021-12-27 13:04:09 -0600

    jwt and service abstraction example

---
+++ Models/UserLogins.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+namespace Models
+{
+    public class UserLogins
+    {
+        [Required]
+        public string UserName
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+        public UserLogins() { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,46 @@
 using Microsoft.OpenApi.Models;
+using Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// builder.Services.AddJWTTokenServices(builder.Services, builder.Configuration);
+AddJWTTokenServicesExtensions.AddJWTTokenServices(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+
+// To check web api with swagger we need to add authorization security options in swagger, 
+// so used AddSecurityDefination() and AddSecurityRequirement() function to add Security options.
+builder.Services.AddSwaggerGen(options =>
 {
-     c.SwaggerDoc("v1", new OpenApiInfo {
-         Title = "PizzaStore API",
-         Description = "Making the Pizzas you love",
-         Version = "v1" });
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStore API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme {
+                    Reference = new Microsoft.OpenApi.Models.OpenApiReference {
+                        Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                    }
+                },
+                new string[] {}
+        }
+    });
 });
 
 builder.Services.AddTransient<Services.IPizzaService, Services.PizzaService>();
